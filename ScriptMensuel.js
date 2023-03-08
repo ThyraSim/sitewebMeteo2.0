50bfef414227e3dd339b9889686e63df190ca0e6
Jean-Dan10 <jean.daniel.laplante@gmail.com>
2023-03-08 13:55:36 -0500

    utilisation fonction et commentaire

--- ScriptMensuel.js
+++ ScriptMensuel.js
@@ -1,18 +1,12 @@
-//Prise en mémoire de la date d'aujourd'hui
+//Prise en mémoire de la date d'aujourd'hui pour vérifier le mois en cours
 var today = new Date();
 today.setHours(0, 0, 0, 0);
 
-// cible dans le html
+// dropdown menu
 
-var tabCalendrier = document.getElementById("tabCalendrier"); // table calendrier
 var ListeMois = document.getElementById("ListeMois"); //  dropdown menu
 
-//cible html pour  afficher statistique
-var min = document.getElementById("min");
-var max = document.getElementById("max");
-var moy = document.getElementById("moy");
-
-// récupère le mois en cours et défini la valeur du dropbox par defaut
+// récupère le mois en cours et défini la valeur du dropdown menu par defaut
 var moisEnCours = today.getMonth();
 ListeMois.value = moisEnCours;
 
@@ -21,21 +15,18 @@ fetchDataForMonth(moisEnCours);
 
 // lorsque change de valeur dans le dropdown menu la value ( 0,1,2 ...) est saisi en paramètre
 ListeMois.addEventListener("change", () => {
-  // vider le tableau
-  tabCalendrier.innerHTML = "";
-
   fetchDataForMonth(ListeMois.value);
 });
 
-// Fonction pour sortir exploiter les données JSON, calculer statistique et générer le calendrier selon le mois choisi
+// Fonction pour sortir récupérer les données JSON, calculer statistique et générer le calendrier selon le mois choisi
 function fetchDataForMonth(mois) {
   fetch("temperatures_2023.json")
     .then((response) => response.json())
     .then((data) => {
       temp = data.temperatures;
-
-      // récupère les données juste pour le mois dans tabTempMois
-      CreerTabTempMois(mois,temp)
+      
+      // RÉCUPÈRE LES DONNES JUSTE POUR LE MOIS DANS tabTempMois
+      tabTempMois = CreerTabTempMois(mois,temp)
 
       // CALCUL STATISTIQUE ET AFFICHAGE pour le mois en cours ( MIN, MAX , MOY)
       afficherStatistique(tabTempMois);
@@ -46,6 +37,7 @@ function fetchDataForMonth(mois) {
     });
 }
 
+// récupère les données juste pour le mois dans tabTempMois
 function CreerTabTempMois(mois,temp){
 
      // récupère les données juste pour le mois
@@ -60,10 +52,18 @@ function CreerTabTempMois(mois,temp){
        }
      });
 
+     return tabTempMois
+
 
 }
 
+ // CALCUL STATISTIQUE ET AFFICHAGE pour le mois en cours ( MIN, MAX , MOY)
 function afficherStatistique(tabTempMois) {
+  //cible html pour  afficher statistique
+var min = document.getElementById("min");
+var max = document.getElementById("max");
+var moy = document.getElementById("moy");
+  
   // CALCUL STATISTIQUE ET AFFICHAGE ( MIN, MAX , MOY)
   max.innerHTML = Math.max(...tabTempMois); // calcul de la valeur max du mois
   min.innerHTML = Math.min(...tabTempMois); // calcul de la valeur min
@@ -76,19 +76,20 @@ function afficherStatistique(tabTempMois) {
   moy.innerHTML = Math.round(sum / nb);
 }
 
+// //GÉNÉRER CALENDRIER selon le mois choisi
 function genereCalendrier(annee, mois, temp) {
   
 
     // Get a reference to the calendar body
-  const tabCalendrier = document.getElementById("tabCalendrier");
+  const tableCalendrier = document.getElementById("tableCalendrier");
 
   // Clear the previous contents of the calendar
-  tabCalendrier.innerHTML = "";
+  tableCalendrier.innerHTML = "";
 
   // Get the first day of the month
   var firstDay = new Date(annee, mois, 1).getDay();
   // Get the number of days in the month
-  const lastDay = new Date(annee, mois + 1, 0).getDate();
+  var lastDay = new Date(annee, mois + 1, 0).getDate();
 
   // Loop through each row of the calendar
   for (let i = 0; i < 6; i++) {
@@ -97,8 +98,12 @@ function genereCalendrier(annee, mois, temp) {
 
     // Loop through each column of the row
     for (let j = 0; j < 7; j++) {
-      // Calculate the day number for this cell
+      // Calcul pour quel jour on commence a remplir la table et quel case
       const day = i * 7 + j - firstDay + 1; 
+      //exemple mars 2023 first day = 3
+      //0*7+0-3 +1 = -2 avec le if ci-dessous (if (day > 0 && day <= lastDay) )pas d'affichage dans la première case(dimanche) jusqu'à i=3 ( quatrième case = mercredi)
+      // i = 0 et j = 3 -> 0*7+3-3+1 = 1 => day = 1 donc avec cell.innerText =  day + ... => 1 ;
+
 
       // Create a new cell
       const cell = document.createElement("td");
@@ -108,7 +113,7 @@ function genereCalendrier(annee, mois, temp) {
         // Find the temperature for this day
         let temperature = "";
         for (let k = 0; k < temp.length; k++) {
-          // parcours le tableau "temp" avec toute les données du JSON "temp = data.temperatures;"" si date match on récupère la temperature
+          // parcours le tableau "temp" avec toute les données du JSON "temp = data.temperatures;"" si date match on récupère la temperature du jour
           const dateJSON = new Date(temp[k].DateDuJour);
           dateJSON.setHours(0, 0, 0, 0);
           dateJSON.setDate(dateJSON.getDate() + 1);
@@ -123,12 +128,12 @@ function genereCalendrier(annee, mois, temp) {
           }
         }
 
-        cell.innerText = "" + day + "\n" + temperature;
-        var icone1 = document.createElement("img");
-        chooseIcon(temperature); //Pour l'icône niege, pluie, etc
-        icone1.src = icone;
-        icone1.width = "10"; //Taille de l'icône
-        cell.appendChild(icone1);
+        cell.innerText =  day + "\n" + temperature;
+        var icone = document.createElement("img");
+         //Pour l'icône niege, pluie, etc
+        icone.src =  chooseIcon(temperature)
+        icone.width = "10"; //Taille de l'icône
+        cell.appendChild(icone);
       }
 
       // Add the cell to the row
@@ -136,22 +141,21 @@ function genereCalendrier(annee, mois, temp) {
     }
 
     // Add the row to the calendar body
-    tabCalendrier.appendChild(row);
+    tableCalendrier.appendChild(row);
   }
 }
 
 //fonction reprise du code de simon
 
-
 //Choix de l'icône
 function chooseIcon(temperature) {
   if (temperature <= 0) {
-    icone = "images/neige.png";
+    return "images/neige.png";
   } else if (temperature >= 20) {
-    icone = "images/soleil.png";
+    return "images/soleil.png";
   } else if (temperature <= 10) {
-    icone = "images/pluie.png";
+    return "images/pluie.png";
   } else if (temperature < 20) {
-    icone = "images/nuage.png";
+    return "images/nuage.png";
   }
 }
